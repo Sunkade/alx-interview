4d1c07e5d85bd1e06a0c6f441d07e9fff0b223de
Sunkade <sunkadefasola@gmail.com>
2024-05-09 18:59:36 +0100

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -12,16 +12,16 @@ def island_perimeter(grid):
         the perimeter of the island
     """
 
-    d = 0
-    for c in range(len(grid)):
+    p = 0
+    for i in range(len(grid)):
         for j in range(len(grid[i])):
             if (grid[i][j] == 1):
-                if (c <= 0 or grid[i - 1][j] == 0):
-                    d += 1
-                if (c >= len(grid) - 1 or grid[i + 1][j] == 0):
-                    d += 1
+                if (i <= 0 or grid[i - 1][j] == 0):
+                    p += 1
+                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
+                    p += 1
                 if (j <= 0 or grid[i][j - 1] == 0):
-                    d += 1
+                    p += 1
                 if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
-                    d += 1
-    return d
+                    p += 1
+    return p
