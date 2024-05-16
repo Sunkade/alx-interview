d5e2042eff3943d066e12bca74fa13bfd5bdf89a
Sunkade <sunkadefasola@gmail.com>
2024-05-16 16:32:47 +0100

    0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python3
-"""
-Pascal's Triangle
-"""
+"""Prime Game"""
 
-def pascal_triangle(n):
-    """Returns a list of integers representing the pascal's triangle"""
-    if n <= 0:
-        return []
-    a = [[] for i in range(n)]
-    for i in range(n):
-        for j in range(i + 1):
-            if j < i:
-                if j == 0:
-                    a[i].append(1)
-                else:
-                    a[i].append(a[i - 1][j] + a[i - 1][j - 1])
-            elif j == i:
-                a[i].append(1)
-    return a
+def is_prime(n):
+    """Check if a number is prime"""
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def isWinner(x, nums):
+    """Determine the winner of each round"""
+    if not nums or x < 1:
+        return None
+    
+    ben_wins = 0
+    for n in nums:
+        prime_count = sum(1 for i in range(2, n + 1) if is_prime(i))
+        if prime_count % 2 == 0:
+            ben_wins += 1
+
+    if ben_wins > x // 2:
+        return "Ben"
+    elif ben_wins < x // 2:
+        return "Maria"
+    else:
+        return None
